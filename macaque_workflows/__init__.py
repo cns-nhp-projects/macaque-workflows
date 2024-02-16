5f071fd686869fee2747f232d694ab77eec85a87
Dan Gale <danjgale@gmail.com>
2024-02-16 14:57:33 -0700

    added key workflows

---
+++ macaque_workflows/__init__.py
@@ -0,0 +1,4 @@
+from macaque_workflows.workflows.coregister import init_coregistration_wf
+from macaque_workflows.workflows.downsample import init_downsample_wf
+
+__all__ = ["init_coregistration_wf", "init_downsample_wf"]
