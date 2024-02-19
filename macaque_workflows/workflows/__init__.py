cc0e9ef47abe7db284f03ea6f8737b3399e1b528
Dan Gale <danjgale@gmail.com>
2024-02-19 10:52:13 -0700

    added nmt wf

--- macaque_workflows/workflows/__init__.py
+++ macaque_workflows/workflows/__init__.py
@@ -1,4 +1,5 @@
 from macaque_workflows.workflows.coregister import init_coregistration_wf
 from macaque_workflows.workflows.downsample import init_downsample_wf
+from macaque_workflows.workflows.nmt import init_nmt_wf
 
-__all__ = ["init_coregistration_wf", "init_downsample_wf"]
+__all__ = ["init_coregistration_wf", "init_downsample_wf", "init_nmt_wf"]
