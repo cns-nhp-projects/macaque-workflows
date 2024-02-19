8585f1f9ae44736e535d7a2e2e4e85027f27b3e4
Dan Gale <danjgale@gmail.com>
2024-02-18 17:56:13 -0700

    moved workflow funcs into workflows init

--- macaque_workflows/__init__.py
+++
@@ -1,4 +0,0 @@
-from macaque_workflows.workflows.coregister import init_coregistration_wf
-from macaque_workflows.workflows.downsample import init_downsample_wf
-
-__all__ = ["init_coregistration_wf", "init_downsample_wf"]
